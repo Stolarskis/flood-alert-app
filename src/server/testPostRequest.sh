e8a65f5b2813a6a51bb65d6080bf83a2c96e96de
Stolarskis <csstolarski@gmail.com>
2018-10-30 11:58:14 -0400

    Added script to test if the server is able to decode the addUser to post request

---
+++ src/server/testPostRequest.sh
@@ -0,0 +1,6 @@
+
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name":"test-name","email":"Test-Email","phone":"56325234242"}' \
+  http://localhost:3000/addUser
